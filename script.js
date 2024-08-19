a73e347ce6f99a66c5fb5f99b23954d5a52ed820
Francesco-Matteucci <francescomatteucci@gmail.com>
2024-08-19 14:13:50 +0200

    fake login done

--- script.js
+++ script.js
@@ -27,3 +27,49 @@ if (userRoll > computerRoll) {
 
 //Stampo il vincitore in pagina
 winner.innerText = result
+
+
+//? --------------------------------------------------------------------------------------------------------------------
+
+//! ESERCIZIO 2 - Finta Login
+// Inventa una lista di email autorizzate
+// Chiedi all’utente la sua email, con un piccolo form.
+// Controlla che sia nella lista di chi può accedere,
+// Stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Invento una lista di mail autorizzate
+const authorizedEmails = ["francesco@boolean.com", "angelo@boolean.com", "giorgio@boolean.com", "giovanni@boolean.com", "marco@boolean.com"];
+
+
+// Recupero elementi dal DOM
+const loginButton = document.getElementById("loginButton");
+const messageElement = document.getElementById("message");
+
+// Aggiungo un listener per l'evento click del bottone del form
+loginButton.addEventListener('click', function () {
+
+    //Recupero la mail inserita dall'utente nell'input del form
+    const userEmail = document.getElementById("email").value;
+
+
+    //Creo una variabile per determinare se la mail inserita dall'utente è autorizzata al login
+    let isAuthorized = false;
+
+    // Creo un ciclo "for" per controllare se la mail è presente nella lista autorizzata
+    for (let i = 0; i < authorizedEmails.length; i++) {
+        if (userEmail === authorizedEmails[i]) {
+            isAuthorized = true;
+            break;
+        }
+    }
+
+    // Stampo il messaggio per l'utente a seconda della validazione o meno all'accesso
+    if (isAuthorized) {
+        messageElement.innerText = "Accesso consentito. Benvenuto!";
+        messageElement.style.color = "green";
+    } else {
+        messageElement.innerText = "Accesso negato. Email non autorizzata.";
+        messageElement.style.color = "red";
+    }
+});
